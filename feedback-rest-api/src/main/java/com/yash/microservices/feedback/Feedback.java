4c3b20e0b7246a713a1bddb9ad5bdde888aeab22
YASH\vishal.kumkar <vishal.kumkar@yash.com>
2018-12-18 17:42:36 +0530

    Initial Commit

---
+++ feedback-rest-api/src/main/java/com/yash/microservices/feedback/Feedback.java
@@ -0,0 +1,39 @@
+package com.yash.microservices.feedback;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.hateoas.Identifiable;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Feedback implements Identifiable<Long> {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String username;
+    private String source;
+    private Integer rating;
+    private String comment;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdTime;
+
+    @PrePersist
+    protected void onCreate() {
+        createdTime = new Date();
+    }
+
+    @Override
+    public Long getId() {
+        return this.id;
+    }
+}
